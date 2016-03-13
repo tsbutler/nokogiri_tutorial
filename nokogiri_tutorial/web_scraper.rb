ca4b84fc0a7d8787f7724356f1e7e01dedfabe02
Thorin Butler <thorin.butler@gmail.com>
2016-03-13 16:28:24 -0500

    Set put the GET request and initialized Pry for the project

--- nokogiri_tutorial/web_scraper.rb
+++ nokogiri_tutorial/web_scraper.rb
@@ -3,3 +3,8 @@ require 'Nokogiri'
 require 'JSON'
 require 'Pry'
 require 'csv'
+
+#This is how we request the page we're going to scrape.
+page = HTTParty.get('https://newyork.craigslist.org/search/pet?s=0')
+
+Pry.start(binding)
