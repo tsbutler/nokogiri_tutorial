03e30fd2cba0c070b1acc492a537548f7e13c145
Thorin Butler <thorin.butler@gmail.com>
2016-03-13 16:36:59 -0500

    added variable parse_page

--- nokogiri_tutorial/web_scraper.rb
+++ nokogiri_tutorial/web_scraper.rb
@@ -7,4 +7,6 @@ require 'csv'
 #This is how we request the page we're going to scrape.
 page = HTTParty.get('https://newyork.craigslist.org/search/pet?s=0')
 
+parse_page = Nokogiri::HTML(page)
+
 Pry.start(binding)
