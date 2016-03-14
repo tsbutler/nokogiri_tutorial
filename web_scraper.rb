c97845f6c15ab9bb40175f597ebc73fbd2c58cd4
Thorin Butler <thorin.butler@gmail.com>
2016-03-14 10:43:47 -0500

    first commit

---
+++ web_scraper.rb
@@ -0,0 +1,27 @@
+require 'HTTParty'
+require 'Nokogiri'
+require 'JSON'
+require 'Pry'
+require 'csv'
+
+#This is how we request the page we're going to scrape.
+page = HTTParty.get('https://newyork.craigslist.org/search/pet?s=0')
+
+#This is where we transform our http response into a nokogiri object
+parse_page = Nokogiri::HTML(page)
+
+#This is an empty array where we will store all the craigslist pets
+pets_array = []
+
+#This is where we parse the data
+parse_page.css('.content').css('.row').css('.hdrlnk').map do |a|
+  post_name = a.text
+  pets_array.push(post_name)
+end
+
+#This will push your array into a csv file
+CSV.open('pets.csv', 'w') do |csv|
+  csv << pets_array
+end
+
+#Pry.start(binding)
